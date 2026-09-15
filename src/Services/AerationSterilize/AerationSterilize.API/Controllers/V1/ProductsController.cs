fc49700d56d39e76c5f5fa016600474a355a14d1
NoobDev9607 <hanoi-is6@asahi-intecc.com>
2026-09-15 09:57:15 +0700

    Thêm API UpdateProductByName

--- src/Services/AerationSterilize/AerationSterilize.API/Controllers/V1/ProductsController.cs
+++ src/Services/AerationSterilize/AerationSterilize.API/Controllers/V1/ProductsController.cs
@@ -75,4 +75,14 @@ public class ProductsController : ApiController
         var result = await Sender.Send(command);
         return Ok(result);
     }
+
+    [HttpPut("{productName}")]
+    [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateProductByName(string productName, [FromBody] UpdateProductCommand command)
+    {
+        command.SetName(productName);
+        var result = await Sender.Send(command);
+        return Ok(result);
+    }
 }
