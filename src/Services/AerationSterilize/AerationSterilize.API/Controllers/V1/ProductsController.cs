539cc51fbe4b051c3f428c9a41af42e009e7234e
Ms.Hong AIX <hanoi-eng81@asahi-intecc.com>
2026-09-15 09:54:32 +0700

    abc

--- src/Services/AerationSterilize/AerationSterilize.API/Controllers/V1/ProductsController.cs
+++ src/Services/AerationSterilize/AerationSterilize.API/Controllers/V1/ProductsController.cs
@@ -65,11 +65,11 @@ public class ProductsController : ApiController
         var result = await Sender.Send(new DeleteProductCommand(productId));
         return Ok(result);
     }
-
+    // hong sửa
     [HttpPut("{productId}")]
     [ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> UpdateProductById(Guid productId, [FromBody] UpdateProductCommand command)
+    public async Task<IActionResult> UpdateProductByIdABC(Guid productId, [FromBody] UpdateProductCommand command)
     {
         command.SetId(productId);
         var result = await Sender.Send(command);
